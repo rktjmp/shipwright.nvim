850c150f8e4fd8a5891b695b613606358604eb5e
Oliver Marriott <hello@omarriott.com>
2021-11-05 21:17:50 +1100

    Add contrib table content checks to build_file test

--- spec/build/build_file.lua
+++ spec/build/build_file.lua
@@ -1,10 +1,16 @@
--- check we have the expected imports
+-- core
 assert(type(run) == "function")
 assert(type(branch) == "function")
 assert(type(overwrite) == "function")
 assert(type(patchwrite) == "function")
 assert(type(prepend) == "function")
 assert(type(append) == "function")
+
+-- contrib
 assert(type(contrib) == "table")
+assert(type(contrib.wezterm) == "function")
+assert(type(contrib.alacritty) == "function")
+assert(type(contrib.kitty) == "function")
+
 -- can still access normal stuff
 assert(type(vim) == "table")
