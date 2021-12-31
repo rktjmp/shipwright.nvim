4bcce3b55709a605f22ebe8b6de6b1dabe8c60ae
Thuan Pham <phamtanthuan2311@gmail.com>
2021-12-31 11:22:32 +0700

    🐞fix(windows terminal contrib): follow Microsoft format

--- lua/shipwright/transform/contrib/windows_terminal.lua
+++ lua/shipwright/transform/contrib/windows_terminal.lua
@@ -1,55 +1,62 @@
 --- windows terminal transform, expects a table in the shape:
 --
 -- @param colors {
--- "name": "Theme Name",
--- "foreground": "#000000",
--- "background": "#000000",
--- "selectionBackground": "#000000",
+-- "name" : "Theme Name",
+
 -- "cursorColor": "#000000",
--- "black": "#000000",
--- "red": "#000000",
--- "green": "#000000",
--- "yellow": "#000000",
--- "blue": "#000000",
--- "purple": "#000000",
--- "cyan": "#000000",
--- "white": "#000000",
--- "brightBlack": "#000000",
--- "brightRed": "#000000",
--- "brightGreen": "#000000",
--- "brightYellow": "#000000",
--- "brightBlue": "#000000",
--- "brightPurple": "#000000",
--- "brightCyan": "#000000",
--- "brightWhite": "#000000"
+-- "selectionBackground": "#000000",
+
+-- "background" : "#000000",
+-- "foreground" : "#000000",
+
+-- "black" : "#000000",
+-- "blue" : "#000000",
+-- "cyan" : "#000000",
+-- "green" : "#000000",
+-- "purple" : "#000000",
+-- "red" : "#000000",
+-- "white" : "#000000",
+-- "yellow" : "#000000",
+-- "brightBlack" : "#000000",
+-- "brightBlue" : "#000000",
+-- "brightCyan" : "#000000",
+-- "brightGreen" : "#000000",
+-- "brightPurple" : "#000000",
+-- "brightRed" : "#000000",
+-- "brightWhite" : "#000000",
+-- "brightYellow" : "#000000"
 -- }
 
 local template = [[{
     "name": "$name",
-    "foreground": "$fg",
-    "background": "$bg",
+
     "selectionBackground": "$selection_bg",
     "cursorColor": "$cursor_bg",
+
+    "foreground": "$fg",
+    "background": "$bg",
+
     "black": "$black",
-    "red": "$red",
-    "green": "$green",
-    "yellow": "$yellow",
     "blue": "$blue",
-    "purple": "$magenta",
     "cyan": "$cyan",
+    "green": "$green",
+    "purple": "$magenta",
+    "red": "$red",
     "white": "$white",
+    "yellow": "$yellow",
     "brightBlack": "$bright_black",
-    "brightRed": "$bright_red",
-    "brightGreen": "$bright_green",
-    "brightYellow": "$bright_yellow",
     "brightBlue": "$bright_blue",
-    "brightPurple": "$bright_magenta",
     "brightCyan": "$bright_cyan",
+    "brightGreen": "$bright_green",
+    "brightRed": "$bright_red",
+    "brightPurple": "$bright_magenta",
     "brightWhite": "$bright_white"
+    "brightYellow": "$bright_yellow",
 }]]
 
 local helpers = require("shipwright.transform.helpers")
 local check_keys = {
+  "$name",
   "fg", "bg",
   "cursor_bg",
   "selection_bg",
